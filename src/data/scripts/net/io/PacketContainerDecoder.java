1ea236f6d7bc25b643b3d30b729e9c56e3ee85cf
BuildTools <unconfigured@null.spigotmc.org>
2022-05-06 11:28:59 +0800

    decoder update

--- src/data/scripts/net/io/PacketContainerDecoder.java
+++ src/data/scripts/net/io/PacketContainerDecoder.java
@@ -7,25 +7,13 @@ import io.netty.handler.codec.ReplayingDecoder;
 import java.util.List;
 
 public class PacketContainerDecoder extends ReplayingDecoder<DecodeState> {
-    private int length;
-
     public PacketContainerDecoder() {
         super(DecodeState.READ_LENGTH);
     }
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
-        switch (state()) {
-            case READ_LENGTH:
-                length = in.readInt();
-                checkpoint(DecodeState.READ_CONTENT);
-            case READ_CONTENT:
-                ByteBuf frame = in.readBytes(length);
-                checkpoint(DecodeState.READ_LENGTH);
-                out.add(frame);
-                break;
-            default:
-                throw new Error("bugger");
-        }
+        int length = in.readInt();
+        out.add(in.readBytes(length));
     }
 }
