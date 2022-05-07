7984be41faa72e13e279a81561985f2cb844c3e5
BuildTools <unconfigured@null.spigotmc.org>
2022-05-08 00:06:32 +0800

    more debug

--- src/data/scripts/net/io/PacketDecoder.java
+++ src/data/scripts/net/io/PacketDecoder.java
@@ -21,22 +21,31 @@ public class PacketDecoder extends ByteToMessageDecoder {
 
         int tick = in.readInt();
 
+        Unpacked unpacked = getUnpacked(in, tick);
+
+        int readable = in.readableBytes();
+        if (readable > 0) throw new OutOfMemoryError(in.readableBytes() + " bytes left in packet decoder frame");
+
+        out.add(unpacked);
+    }
+
+    private Unpacked getUnpacked(ByteBuf in, int tick) {
         // integer keys are unique instance IDs
         Map<Integer, APackable> entities = new HashMap<>();
         // integer keys are unique record IDs
         Map<Integer, ARecord<?>> records = new HashMap<>();
 
-        int type = in.readInt();
-        int entityID = type;
+        int entityID = in.readInt();
         int entityInstanceID = in.readInt();
 
         while (in.readableBytes() > 0) {
-            type = in.readInt();
+            int type = in.readInt();
 
             if (DataManager.entityTypeIDs.containsValue(type)) {
                 // reached new entity
-                if (records.isEmpty()) throw new NullPointerException("Entity read zero records: " + entityID);
-                entities.put(entityInstanceID, getEntity(entityID, records));
+                if (records.isEmpty()) throw new NullPointerException("Entity read zero records: " + entityInstanceID);
+                APackable entity = DataManager.entityFactory(entityID).unpack(entityInstanceID, records);
+                entities.put(entityInstanceID, entity);
 
                 entityID = type;
                 entityInstanceID = in.readInt();
@@ -46,18 +55,10 @@ public class PacketDecoder extends ByteToMessageDecoder {
                 records.put(uniqueID, DataManager.recordFactory(type).read(in));
             }
         }
-//        if (records.isEmpty()) throw new NullPointerException("Entity read zero records: " + entityID);
-//        entities.put(entityInstanceID, getEntity(type, records));
+        if (records.isEmpty()) throw new NullPointerException("Entity read zero records: " + entityInstanceID);
+        APackable entity = DataManager.entityFactory(entityID).unpack(entityInstanceID, records);
+        entities.put(entityInstanceID, entity);
 
-        Unpacked unpacked = new Unpacked(entities, tick);
-
-        int readable = in.readableBytes();
-        if (readable > 0) throw new OutOfMemoryError(in.readableBytes() + " bytes left in packet decoder frame");
-
-        out.add(unpacked);
-    }
-
-    private APackable getEntity(int id, Map<Integer, ARecord<?>> records) {
-        return DataManager.entityFactory(id).unpack(id, records);
+        return new Unpacked(entities, tick);
     }
 }
