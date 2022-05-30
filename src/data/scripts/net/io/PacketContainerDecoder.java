62894af6fb9896e4269cd6d2e845273d18bea668
BuildTools <unconfigured@null.spigotmc.org>
2022-05-30 14:10:52 +0800

    decode fix

--- src/data/scripts/net/io/PacketContainerDecoder.java
+++ src/data/scripts/net/io/PacketContainerDecoder.java
@@ -10,6 +10,9 @@ public class PacketContainerDecoder extends ReplayingDecoder<Object> {
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
         int length = in.readInt();
+        if (in.readableBytes() < length) return;
+        else if (in.readableBytes() > length) throw new IndexOutOfBoundsException("Readable bytes exceeds header value");
+
         ByteBuf frame = in.readBytes(length);
         out.add(frame);
     }
