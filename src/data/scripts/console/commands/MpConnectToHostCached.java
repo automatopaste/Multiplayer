ae0c3bcf0f02cb6a36f404bf90d2734c626184c5
automatopaste <raner9922@gmail.com>
2022-09-06 20:38:32 +0800

    debug

--- src/data/scripts/console/commands/MpConnectToHostCached.java
+++ src/data/scripts/console/commands/MpConnectToHostCached.java
@@ -6,27 +6,18 @@ import org.jetbrains.annotations.NotNull;
 import org.lazywizard.console.BaseCommand;
 import org.lazywizard.console.Console;
 
-public class mpConnectToHost implements BaseCommand {
+public class MpConnectToHostCached implements BaseCommand {
     @Override
     public CommandResult runCommand(@NotNull String args, @NotNull CommandContext context) {
         if (context != CommandContext.COMBAT_SIMULATION) {
             Console.showMessage("Command only usable in simulation");
             return CommandResult.ERROR;
         }
-        if (args.trim().isEmpty()) {
-            Console.showMessage("Specify address");
-            return CommandResult.BAD_SYNTAX;
-        }
 
-        String[] ids = args.split(" ");
-        if (ids.length != 1) {
-            Console.showMessage("Syntax error");
-            return CommandResult.BAD_SYNTAX;
-        }
-
-        String[] address = ids[0].split(":");
-        String host = address[0];
-        int port = Integer.parseInt(address[1]);
+        String a = Global.getSettings().getString("mpHost");
+        String[] b = a.split(":");
+        String host = b[0];
+        int port = Integer.parseInt(b[1]);
 
         Console.showMessage("Starting client on port " + port);
         Global.getCombatEngine().addPlugin(new MPClientPlugin(host, port));
