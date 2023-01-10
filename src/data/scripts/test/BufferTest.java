5269a7a1bf9861713587e8cf67ccb4a678e3e0b4
tomatopaste <raner9922@gmail.com>
2023-01-10 22:40:10 +0800

    debug

---
+++ src/data/scripts/test/BufferTest.java
@@ -0,0 +1,19 @@
+package data.scripts.test;
+
+import data.scripts.net.data.records.BaseRecord;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BufferTest {
+
+    private final Map<Byte, Map<Short, Map<Byte, BaseRecord<?>>>> data;
+
+    public BufferTest() {
+        data = new HashMap<>();
+    }
+
+    public void run() {
+
+    }
+}
